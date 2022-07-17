5ed9b79a3df2dc0a2c4fc71bafa04ead5a660dd7
Lucas Gomes <lgxm3z@gmail.com>
2022-07-17 17:35:30 -0300

    craco tests

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,12 +1,16 @@
-//@ts-ignore
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "@piano-demo/core/src/App";
 import reportWebVitals from "./reportWebVitals";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
