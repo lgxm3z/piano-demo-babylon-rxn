0bd1901549ef59092fe31beab21ca8ef37a86c4a
Lucas <lgxm3z@gmail.com>
2022-07-22 19:53:44 -0300

    format App

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,16 +1,21 @@
-import { Color4, Engine, Scene, UniversalCamera, Vector3 } from "@babylonjs/core";
+import {
+  Color4,
+  Engine,
+  Scene,
+  UniversalCamera,
+  Vector3,
+} from "@babylonjs/core";
 import React, { useCallback } from "react";
 import { Text, View } from "react-native";
 import GLRenderer from "./components/GLRenderer";
 
 export default function App() {
-
   const onCreateEngine = useCallback((engine: Engine | undefined) => {
     if (!engine) return;
 
     const scene = new Scene(engine);
-    scene.clearColor = Color4.FromHexString('#000000');
-    const camera = new UniversalCamera('Camera', new Vector3(0, 0, 0), scene);
+    scene.clearColor = Color4.FromHexString("#000000");
+    const camera = new UniversalCamera("Camera", new Vector3(0, 0, 0), scene);
 
     engine.runRenderLoop(function () {
       scene.render();
@@ -21,18 +26,18 @@ export default function App() {
       camera.dispose();
       engine.dispose();
     };
-  },[ ])
-  
+  }, []);
+
   return (
     <View
-    style={{
-      flex: 1,
-      backgroundColor: '#000000',
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-    }}
-  >
+      style={{
+        flex: 1,
+        backgroundColor: "#000000",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+      }}
+    >
       <GLRenderer onCreateEngine={onCreateEngine} />
     </View>
   );
